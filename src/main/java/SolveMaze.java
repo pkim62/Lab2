02e2a511ce6b0c1367af372e81d85afc18bc5e19
pkim62 <pkim62@illinois.edu>
2018-09-12 20:44:06 -0500

    done with maze

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -35,12 +35,32 @@ public class SolveMaze {
          */
         for (int step = 0; step < 1000; step++) {
             // Implement your maze solving algorithm here
-        }
+            while (!maze.isFinished()) {
+                maze.turnLeft();
+                if (maze.canMove()) {
+                    maze.move();
+                } else {
+                    maze.turnRight();
+                    if (maze.canMove()) {
+                        maze.move();
+                    } else {
+                        maze.turnRight();
+                        if (maze.canMove())
+                            maze.move();
+                        else
+                            maze.turnRight();
+                        maze.move();
+                    }
 
-        if (maze.isFinished()) {
-            System.out.println("You solved the maze!");
-        } else {
-            System.out.println("Try again!");
+
+                    if (maze.isFinished()) {
+                        System.out.println("You solved the maze!");
+                    } else {
+                        System.out.println("Try again!");
+                    }
+                }
+            }
         }
     }
 }
+
